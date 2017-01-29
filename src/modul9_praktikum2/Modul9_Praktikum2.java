6bdb5b3dc6afd6cf741031084e10ea210e3a7ad3
septy <septydarmantya@gmail.com>
2017-01-29 09:04:39 +0700

    Praktikum 2

---
+++ src/modul9_praktikum2/Modul9_Praktikum2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul9_praktikum2;
+
+/**
+ *
+ * @author asus
+ */
+public class Modul9_Praktikum2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
